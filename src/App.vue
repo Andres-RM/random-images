42622de10eff4c6cd451977862d6f211efb54465
Luis Andres Ramirez Matheus <ramirezandres1994@gmail.com>
2025-04-02 19:37:17 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -35,12 +35,7 @@ onMounted(() => {
 <template>
   <main class="page">
     <div class="container mx-auto px-4">
-      <div class="flex flex-col py-14 items-center">
-        <h2
-          class="scroll-m-20 text-center pb-2 text-5xl font-semibold tracking-tight transition-colors first:mt-0 italic"
-        >
-          Random Images
-        </h2>
+      <div class="flex flex-col py-14 items-center justify-center">
 
         <div class="mt-8 w-full max-w-2xl flex flex-col items-center">
           <div
@@ -53,13 +48,6 @@ onMounted(() => {
               class="w-full h-full object-contain object-center transition-opacity duration-300"
             />
           </div>
-
-          <button
-            @click="getRandomImage"
-            class="mt-6 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
-          >
-            Generate Random Image
-          </button>
         </div>
       </div>
     </div>
